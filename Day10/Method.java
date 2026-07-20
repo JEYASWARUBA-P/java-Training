99f7fb714253a838e1e59d2c587a302207827723
JEYASWARUBA-P <ruba50917@gmail.com>
2026-07-20 21:21:11 +0530

    solved

--- Day10/Method.java
+++ Day10/Method.java
@@ -1,12 +1,5 @@
 package Day10;
 
-class Animal {
-
-    void sound() {
-        System.out.println("Animal makes sound");
-    }
-}
-
 public class Method {
 
     public static void main(String[] args) {
@@ -14,5 +7,11 @@ public class Method {
         Animal a = new Animal();
 
         a.sound();
+    }
+}
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
     }
 }
